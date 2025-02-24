7be84c83148a81f7bd4df11e67bc3f6afa3f4ca5
YK6AWCD29FymA <akochera@uw.edu>
2025-02-24 01:19:19 +0000

    Submitting

--- homework7_aj_kochera.py
+++ homework7_aj_kochera.py
@@ -2,7 +2,7 @@ pizza_orders = ["Mar", "Ita", "Cal"]
 finished_pizzas = []
 while pizza_orders:
         current_pizza = pizza_orders.pop()
-        print(f"Your pizza pie is finished, {current_pizza}")
+        print(f"Your pizza pie is finished, {current_pizza}!")
         finished_pizzas.append(current_pizza)
 
 print("\n")
